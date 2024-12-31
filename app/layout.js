af2af16b6d8215ab19e1846b8a96f459190c1d07
yesutkarsh <contactutkarshverma@gmail.com>
2024-12-31 06:37:16 +0530

    first

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -21,9 +12,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
 
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
 
 
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
